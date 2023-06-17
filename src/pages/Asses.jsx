d7691e51237d131d3246ea91d0e1555af9a85e0c
Sumit Sahu <sahu99516@gmail.com>
2023-06-17 09:41:52 +0530

    fix radio button of mcq, add animation inn assesment circle svg

--- src/pages/Asses.jsx
+++ src/pages/Asses.jsx
@@ -1,125 +1,3 @@
-// import React from "react";
-// import userIcon from "../assets/images/user/userIcon.png";
-
-// import startbutton from "../assets/assess/start-button.svg";
-// import bgImage from "../assets/assess/bg-image.svg";
-
-// function Asses() {
-//   return (
-//     <div
-//       className="bg-slate-500 w-screen h-screen"
-//       style={{ backgroundImage: `url(${bgImage})`, backgroundSize: "cover" }}
-//     >
-//       <div className="w-screen flex justify-between h-44 ">
-//         <h1 className="pt-4 pl-4 md:pt-16 md:pl-16 text-2xl">
-//           Assess Your Mindset
-//         </h1>
-//         <img
-//           src={userIcon}
-//           alt="Current Image"
-//           className="object-contain w-16 h-16 m-3 rounded-full mr-10"
-//         />
-//       </div>
-
-//       <div className="flex w-screen justify-center items-center flex-col h-72 ">
-//         <div className="bg-gray-200 text-center pt-10 pb-10 text-2xl rounded-xl items-center flex flex-col w-fit h-700 h-screen shadow-xl">
-//           <h1>GROWING A GROWTH MINDSET</h1>
-//           <h3 className="pt-6 text-2xl">
-//             Are You Ready for a Positivity Change
-//           </h3>
-//           <h3 className="pt-6 text-xl text-center w-3/4">
-//             Our mindset decides if our character, intelligent and creative
-//             abilities are either are either fixed or can be changed
-//           </h3>
-//           <img
-//             src={startbutton}
-//             alt="startbutton"
-//             className="object-contain absolute w-40 h-40 m-3 rounded-full mr-10 top-1/2"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Asses;
-// import React, { useEffect, useState } from "react";
-// import userIcon from "../assets/images/user/userIcon.png";
-// import startbutton from "../assets/assess/start-button.svg";
-// import bgImage from "../assets/assess/bg-image.svg";
-
-// import newbg from "../assets/assess/newbg.svg";
-
-// function Asses() {
-//   const [zoomLevel, setZoomLevel] = useState(1);
-
-//   useEffect(() => {
-//     const zoomInterval = setInterval(() => {
-//       setZoomLevel((prevZoomLevel) => (prevZoomLevel === 1 ? 1.2 : 1));
-//     }, 2000);
-
-//     return () => clearInterval(zoomInterval);
-//   }, []);
-
-//   return (
-//     <div
-//       className="bg-slate-500 w-screen h-screen"
-//       style={{ backgroundImage: `url(${bgImage})`, backgroundSize: "cover" }}
-//     >
-//       <div className="w-screen flex justify-between h-44 ">
-//         <h1 className="pt-4 pl-4 md:pt-16 md:pl-16 text-2xl">
-//           Assess Your Mindset
-//         </h1>
-//         <img
-//           src={userIcon}
-//           alt="Current Image"
-//           className="object-contain w-16 h-16 m-3 rounded-full mr-10"
-//         />
-//       </div>
-//       {/*
-//       <div className="flex w-screen justify-center items-center flex-col h-80 ">
-//         <div className="bg-white text-center pt-10 pb-20 text-2xl rounded-xl items-center flex flex-col w-fit h-700 h-screen shadow-xl relative">
-//           <div className="absolute top-0 left-1/2 transform -translate-x-1/2">
-//             <div className="absolute -top-3 left-1/2 transform -translate-x-1/2 ">
-//               <div className="h-6 w-96 bg-indexLine rounded-xl"></div>
-//             </div>
-//           </div>
-
-//           <h1>GROWING A GROWTH MINDSET</h1>
-//           <h3 className="pt-6 text-2xl">
-//             Are You Ready for a Positivity Change
-//           </h3>
-//           <h3 className="pt-6 text-xl text-center w-3/4">
-//             Our mindset decides if our character, intelligence, and creative
-//             abilities are fixed or can be changed.
-//           </h3>
-//           <div className="absolute top-56">
-//             <img
-//               src={startbutton}
-//               alt="startbutton"
-//               className="object-contain w-40 h-40 m-3 rounded-full"
-//             />
-//           </div>
-//         </div> */}
-
-//       <div className="flex w-screen justify-center items-center flex-col h-80 relative">
-//         <img
-//           src={newbg}
-//           alt="Current Image"
-//           className="object-full w-800 h-auto m-3"
-//         />
-//         <div className="absolute top-56">
-//           <img
-//             src={startbutton}
-//             alt="startbutton"
-//             className={`object-contain w-40 h-40 m-3 rounded-full transform scale-${zoomLevel}`}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Asses;
 import React, { useEffect, useState } from "react";
 import userIcon from "../assets/images/user/userIcon.png";
 import startbutton from "../assets/assess/start-button.svg";
@@ -131,6 +9,8 @@ import nextImage from "../assets/assess/next-img.svg";
 
 import allquestion from "./questions.json";
 
+import clickSound from "../assets/audio/click.wav";
+
 function Asses() {
   const [zoomLevel, setZoomLevel] = useState(1);
   const dots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -138,6 +18,8 @@ function Asses() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
   const [showResult, setShowResult] = useState(false);
+
+  const clickAudio = new Audio(clickSound);
 
   useEffect(() => {
     const zoomInterval = setInterval(() => {
@@ -158,6 +40,7 @@ function Asses() {
     if (!showResult) {
       setSelectedOptionIndex(optionIndex);
       setShowResult(true);
+      clickAudio.play();
     }
   };
 
@@ -177,7 +60,7 @@ function Asses() {
       let optionClass = "cursor-pointer text-white";
       if (showResult) {
         if (isSelected && isCorrect) {
-          //select and correct
+          // select and correct
           optionClass += " bg-green-500";
         } else if (isSelected && !isCorrect) {
           // select and wrong
@@ -187,22 +70,21 @@ function Asses() {
           optionClass += " bg-green-500";
         }
       }
+
       return (
-        <>
-          <label
-            className={`bg-white text-gray-900 rounded-full shadow-xl p-2 px-4 ${optionClass} `}
-          >
-            <input
-              type="radio"
-              className={optionClass}
-              id="radio"
-              key={index}
-              name="option"
-              onClick={() => handleOptionClick(index)}
-            />
-            <span className="pl-4 text-md">{option}</span>
-          </label>
-        </>
+        <label
+          key={index}
+          className={`flex items-center bg-white text-gray-900 rounded-full shadow-xl p-2 px-4 ${optionClass}`}
+        >
+          <input
+            type="radio"
+            className="mr-2"
+            name="option"
+            checked={isSelected}
+            onChange={() => handleOptionClick(index)}
+          />
+          <span className="text-md">{option}</span>
+        </label>
       );
     });
   };
